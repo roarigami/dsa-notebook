6237c723b9cf1a62f515058184ebbcc2d520dba1
Rory Kane <roarigami@gmail.com>
2023-05-25 16:48:58 -0400

    Has path problem

--- Javascript/graphs.js
+++ Javascript/graphs.js
@@ -1,3 +1,38 @@
+
+//Has path problem 
+
+//depth first 
+const hasPath = (graph, src, dst) => {
+    if(src == dst) return true;
+
+    for(let neighbor of graph[src]) {
+        if(hasPath(graph, neighbor, dst) == true) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+
+//breadth first (iterative obviously)
+const hasPath = (graph, src, dst) => {
+   const queue = [ src ];
+
+   while(queue.length > 0) {
+       const current = queue.shift();
+       if(current == dst) return true;
+
+       for(let neighbor of graph[current]) {
+           queue.push(neighbor);
+       }
+   }
+
+   return false;
+};
+
+
+
 //Undirected Path Graph Problem 
 const undirectedPath = (edges, nodeA, nodeB) => {
     visited = new Set();
