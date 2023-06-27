b1e68bd3c3c1edd0161bcc4eed3516b93e6492db
Rory Kane <roarigami@gmail.com>
2023-06-27 10:29:04 -0400

    Selection sort

--- Javascript/sorting.js
+++ Javascript/sorting.js
@@ -1,11 +1,8 @@
 //Bubble sort 
-
-
-
 const bubbleSort = (array) => {
-    let arrLength = array.length;
-    for(let i = 0; i < arrLength; i++) {
-        for(let j = 0; j < arrLength; j++){
+    let n = array.length;
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < n; j++){
             if(array[j] > array[j + 1]) {
                 let temp = array[j];
                 array[j] = array[j + 1];
@@ -13,9 +10,30 @@ const bubbleSort = (array) => {
             }
         }
     }
-
+    return array;
 }
 let arr = [84,12,65,43,29,94,82,53,4];
-bubbleSort(arr);
+console.log(bubbleSort(arr));
 
-console.log(arr);
+
+//Selection Sort 
+const selectionSort = (array) => {
+    let n = array.length;
+    for(let i = 0; i < n; i++){
+        let min = i;
+        for(let j = i + 1; j < n; j++) {
+            if(array[j] < array[min]) {
+                min = j;
+            }
+        }
+        if(min != i) {
+            let temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+    }
+    return array;
+}
+
+let arr2 = [1, 54, 32, 98, 2132, 09, 143, 1627, 735];
+console.log(selectionSort(arr2));
