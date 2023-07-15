89e05e779168f8e3b066db3cbb8f1274a377b9fd
Rory Kane <roarigami@gmail.com>
2023-07-14 23:40:39 -0400

    Longest Consecutive Sequence

---
+++ Javascript/LCProblem3.js
@@ -0,0 +1,21 @@
+//128. Longest Consecutive Sequence 
+
+const longestConsecutive = (nums) => {
+    let numSet = new Set(nums);
+    let nsLength = numSet.length;
+    longest = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+
+        if(!numSet.has(i - 1)) {
+            length = 0;
+            while(numSet.has(i + length)) {
+                ++length;
+            }
+            longest = Math.max(length, longest);
+        }
+    }
+
+    return longest;
+
+};
