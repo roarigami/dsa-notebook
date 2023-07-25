f8461b3d30a274a19b8691007202ba0bdee46540
Rory Kane <roarigami@gmail.com>
2023-07-24 20:49:41 -0400

    Incomplete / inefficient implementation

---
+++ Cpp/trees.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+using namespace std; 
+
+struct Node { 
+    int data;
+    Node* left;
+    Node* right; 
+};
+
+Node* createNode(int data) {
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->left =newNode->right =nullptr;
+
+    return newNode;
+}
+
+
+int man() {
+
+    Node* root=createNode(1);
+
+    root->left= createNode(2);
+    root->right= createNode(3);
+
+    cin.get();
+
+}
