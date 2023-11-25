913aec67d58a6465c6106663a3b43cba1f38e0ac
Rory Kane <roarigami@gmail.com>
2023-11-25 12:27:40 -0500

    Final working solution. All cases

--- Javascript/LCProblem4.js
+++ Javascript/LCProblem4.js
@@ -4,5 +4,17 @@
 //Tip: Two strings are anagrams if we sort them and get equivalent substrings 
 
 let groupAnagrams = function(strs) {
-    
+    let res = {};
+
+    for(wrd of strs) {
+         let refined = wrd.split("").sort().join("");
+    if (res[refined]) {
+      res[refined].push(wrd);
+    } else {
+      res[refined] = [wrd];
+    }
+  }
+
+return Object.values(res);
+
 };
