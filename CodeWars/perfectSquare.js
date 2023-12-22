97b2c103e29dae05b1bf859c9709395ea307fc05
Rory Kane <roarigami@gmail.com>
2023-12-22 14:21:34 -0500

    Simple perfect square algo long solution

--- CodeWars/perfectSquare.js
+++ CodeWars/perfectSquare.js
@@ -4,5 +4,12 @@
 
 //Longer solution 
 function isSquare(n) {
-   
+    if(n >= 0) {
+        let ans = Math.sqrt(n);
+        if(ans - Math.floor(ans) !== 0 ) {
+          return false;
+        }
+        return ((ans * ans) == n);
+      }
+      return false; // fix me
 }
