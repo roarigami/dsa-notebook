6da8316981757882c328296faa4133c9d27ea64d
Rory Kane <roarigami@gmail.com>
2024-01-20 12:31:29 -0500

    Leetcode generate parenthese problem

--- LeetCode/LCProblem6.js
+++ LeetCode/LCProblem6.js
@@ -1,6 +1,23 @@
 //22. Generate Parentheses 
 // Difficulty: Medium 
 
-let generateParenthesis = function(n) {
+const generateParentheses = (n) => {
+    const res = [];
     
-};
+    function backtrack(stack, openN, closedN) {
+      if (openN === n && closedN === n) {
+        res.push(stack);
+        return;
+      }
+      if (openN < n) {
+        backtrack(stack + "(", openN + 1, closedN);
+      }
+      if (openN > closedN) {
+        backtrack(stack + ")", openN, closedN + 1);
+      }
+    }
+
+    backtrack("", 0, 0);
+    return res;
+    
+  }
